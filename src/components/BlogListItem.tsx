c820d0dc4ac6261d485431310b600f3a6cdb7436
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2025-07-22 01:51:28 -0400

    draft -> publish

--- src/components/BlogListItem.tsx
+++ src/components/BlogListItem.tsx
@@ -3,20 +3,19 @@ import React from 'react';
 import Link from 'next/link';
 
 type BlogListProps = IndexEntry & {
-    writepad?: boolean
+	writepad?: boolean
 }
 
-export const BlogListItem: React.FC<BlogListProps> = ({ draft, title, blurb, date, slug, writepad }) => {
-
-    return (<div className='p-card'>
-        <div className='p-title'>
-            <Link href={`/${writepad ? 'writepad' : 'blog'}/${slug.replace(/\.md$/, '')}`}>
-                <b>{draft ? '[DRAFT] ' : ''}{title}</b>
-            </Link>
-            <div className='slug'>
-                {date}
-            </div>
-        </div>
-        <div>{blurb}</div>
-    </div >);
+export const BlogListItem: React.FC<BlogListProps> = ({ publish, title, blurb, date, slug, writepad }) => {
+	return (<div className='p-card'>
+		<div className='p-title'>
+			<Link href={`/${writepad ? 'writepad' : 'blog'}/${slug.replace(/\.md$/, '')}`}>
+				<b>{!publish ? '[DRAFT] ' : ''}{title}</b>
+			</Link>
+			<div className='slug'>
+				{date}
+			</div>
+		</div>
+		<div>{blurb}</div>
+	</div >);
 }
