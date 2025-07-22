aa9f2849044d6104801c87ec8f2157eafc3ea779
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2025-07-22 10:20:21 -0400

    fix the projects page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,23 +4,23 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 export const Project: React.FC<ProjectData> = ({ title, content, slug, report, languages }) => {
-    return (<div className='p-card'>
-        <div className='p-title'>
-            <b style={{ fontSize: 'large' }}>{title}</b>
-            <div className='slug'>
-                <Link href={`https://github.com/${slug}`}>
-                    {slug}
-                </Link>
-
-            </div>
-            <div className='slug'>
-                {report ?
-                    <Link href={report}>Report</Link> : <></>}
-            </div>
-
-        </div>
-        <div style={{ fontSize: 'small' }}>[{languages}]</div>
-        <ReactMarkdown>{content}</ReactMarkdown>
-        <hr />
-    </div >);
+	return (<div className='p-card'>
+		<div className='p-title'>
+			<b style={{ fontSize: 'large' }}>{title}</b>
+			<div style={{ display: "flex", alignItems: "center", marginTop: "0px" }}>
+				<span className='slug'>
+					<Link href={`https://github.com/${slug}`}>
+						{slug}
+					</Link>
+				</span>
+				<span className='slug'>
+					{report ?
+						<Link href={report}>Report</Link> : <></>}
+				</span>
+			</div>
+		</div>
+		<div style={{ fontSize: 'small' }}>[{languages}]</div>
+		<ReactMarkdown>{content}</ReactMarkdown>
+		<hr />
+	</div >);
 }
