119da2ea3d5cc34d68712564eec20f8110dd7de9
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-01-29 12:50:59 -0500

    Fix lint: 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,32 +4,12 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import React from 'react';
 
-import localFont from 'next/font/local';
-
 export const metadata: Metadata = {
 	title: "Sanchit Sahay",
 	description: "Welcome to my website!",
 };
 
 // src/ui/fonts.ts or app/layout.tsx
-
-export const myCustomFont = localFont({
-	src: [
-		{
-			path: '../../public/Coustard-Regular.ttf', // Adjust path based on your file structure
-			weight: '400',
-			style: 'normal',
-		},
-		{
-			path: '../../public/Coustard-Black.ttf',
-			weight: '700',
-			style: 'normal',
-		},
-	],
-	display: 'swap', // Optional: controls font display behavior
-	variable: '--font-my-custom-font', // Optional: for use with Tailwind CSS
-});
-
 
 export default function RootLayout({
 	children,
