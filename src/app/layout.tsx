212471f16f4ce91dfa55d6b26adb766f5947968f
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-07-10 11:26:24 -0400

    Uribe | News

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "../components/Header";
+import HeaderGate from "../components/HeaderGate";
 import Footer from "../components/Footer";
-import React from 'react';
+import React from "react";
 
 export const metadata: Metadata = {
-	title: "Sanchit Sahay",
-	description: "Welcome to my website!",
+  title: "Sanchit Sahay",
+  description: "Welcome to my website!",
 };
 
 // src/ui/fonts.ts or app/layout.tsx
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className='debug'>
-				<div className='actualbody'>
-					<Header />
-					{children}
-					{/* <div className="debug-grid"></div> */}
-					<Footer />
-				</div>
-				<div className="cmyk start">
-					<div className="c box"></div>
-					<div className="m box"></div>
-					<div className="y box"></div>
-					<div className="k box"></div>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="debug">
+        <div className="actualbody">
+          <HeaderGate />
+          {children}
+          {/* <div className="debug-grid"></div> */}
+          <Footer />
+        </div>
+        <div className="cmyk start">
+          <div className="c box"></div>
+          <div className="m box"></div>
+          <div className="y box"></div>
+          <div className="k box"></div>
+        </div>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,133 +1,162 @@
-import './custom.css';
-import Link from 'next/link';
-import React from 'react';
-import { ReadIndex } from '@/utils';
-import { Metadata } from 'next';
-import Image from 'next/image';
+import "./custom.css";
+import Link from "next/link";
+import React from "react";
+import { ReadIndex } from "@/utils";
+import { Metadata } from "next";
+import Image from "next/image";
 
 const getStaticProps = async () => {
-	const indexEntries: IndexEntry[] = await ReadIndex(true);
+  const indexEntries: IndexEntry[] = await ReadIndex(true);
 
-	const news = ["Catch our talk, <a href=\"https://us.pycon.org/2026/schedule/presentation/116\">Asleep at the Wheel @ PyCon US 2026!</a><br/>"];
+  const news = [
+    [
+      '<a href="https://youtu.be/VYY3HnRtV6U?si=yOaUd6htQnPzMJnE">Asleep at the Wheel (PyCon US 2026)</a> is now on YouTube.',
+      "Uploaded Jul 02, 2026",
+    ],
+    [
+      'Catch our talk, <a href="https://us.pycon.org/2026/schedule/presentation/116">Asleep at the Wheel @ PyCon US 2026!</a>',
+      "Presented May 16, 2026",
+    ],
+  ];
 
-	return { articles: indexEntries.filter(x => x.pinned), news };
-}
+  return { articles: indexEntries.filter((x) => x.pinned), news };
+};
 
-export async function generateMetadata(
-): Promise<Metadata> {
-	return {
-		title: 'Home / Sanchit Sahay'
-	}
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Home / Sanchit Sahay",
+  };
 }
 
 export default async function Home() {
-	const { articles, news } = (await getStaticProps());
+  const { articles, news } = await getStaticProps();
 
-	return (
-		<>
-			<div className='window'>
-				<div className="portrait">
-					<Image
-						src='/mt_washington.jpg'
-						className='window-portrait'
-						width={200}
-						height={200}
-						alt='portrait'
-					/>
-					<figcaption>Mt. Washington, 2025</figcaption>
-				</div>
-				<div className='scrollable'>
-					It was the best of webpages, it was the worst of webpages.
-					<p>
-						I&apos;m an incoming Computer Science PhD student at NYU starting Fall 2026, and a current member of the <Link href="https://ssl.engineering.nyu.edu/">Secure Systems Lab</Link> working on software supply-chain security and operating systems research.
-					</p>
-					<p>
-						In my previous roles, I&apos;ve worked closely on building systems that enhance virtualization and cloud infrastructure, and on improving developer tooling.
-					</p>
-					<p>
-						Check out some of my <Link href="/blog">Pocket Litter</Link> here:
-					</p>
-					<ul>
-						{
-							articles.map(art => (
-								<li key={art.slug} className="pin">
-									<Link href={`/blog/${art.slug}`}>{art.title}</Link>
-									<span> {art.blurb} </span>
-								</li>
-							))
-						}
-					</ul>
-					<table>
-						<tbody className="timeline">
-							<tr>
-								<td>[2024-2026]</td>
-								<td>
-									<b>New York University </b>
-									<div className="mobile"> {/*Ph.D. Computer Science<br />*/}
-										M.S. Computer Science </div>
-								</td>
-								<td className="full">
-									{/*Ph.D. Computer Science<br />*/}
-									M.S. Computer Science
-								</td>
-							</tr>
+  return (
+    <>
+      <div className="window">
+        <div className="portrait">
+          <Image
+            src="/mt_washington.jpg"
+            className="window-portrait"
+            width={200}
+            height={200}
+            alt="portrait"
+          />
+          <figcaption>Mt. Washington, 2025</figcaption>
+        </div>
+        <div className="scrollable">
+          It was the best of webpages, it was the worst of webpages.
+          <p>
+            I&apos;m an incoming Computer Science PhD student at NYU starting
+            Fall 2026, and a current member of the{" "}
+            <Link href="https://ssl.engineering.nyu.edu/">
+              Secure Systems Lab
+            </Link>{" "}
+            working on software supply-chain security and operating systems
+            research.
+          </p>
+          <p>
+            In my previous roles, I&apos;ve worked closely on building systems
+            that enhance virtualization and cloud infrastructure, and on
+            improving developer tooling.
+          </p>
+          <p>
+            Check out some of my <Link href="/blog">Pocket Litter</Link> here:
+          </p>
+          <ul>
+            {articles.map((art) => (
+              <li key={art.slug} className="pin">
+                <Link href={`/blog/${art.slug}`}>{art.title}</Link>
+                <span> {art.blurb} </span>
+              </li>
+            ))}
+          </ul>
+          <table>
+            <tbody className="timeline">
+              <tr>
+                <td>[2024-2026]</td>
+                <td>
+                  <b>New York University </b>
+                  <div className="mobile">
+                    {" "}
+                    {/*Ph.D. Computer Science<br />*/}
+                    M.S. Computer Science{" "}
+                  </div>
+                </td>
+                <td className="full">
+                  {/*Ph.D. Computer Science<br />*/}
+                  M.S. Computer Science
+                </td>
+              </tr>
 
-							<tr>
-								<td>[2022-2024]</td>
-								<td>
-									<b>Commvault Systems </b>
-									<div className="mobile">
-										Virtualization
-									</div>
-								</td>
-								<td className="full">
-									Virtualization
-								</td>
-							</tr>
+              <tr>
+                <td>[2022-2024]</td>
+                <td>
+                  <b>Commvault Systems </b>
+                  <div className="mobile">Virtualization</div>
+                </td>
+                <td className="full">Virtualization</td>
+              </tr>
 
-							<tr>
-								<td>[2021-2021]</td>
-								<td>
-									<b>LegalAI </b>
-									<div className="mobile">
-										Web & DevOps Intern
-									</div>
-								</td>
-								<td className="full">
-									Web & DevOps Intern
-								</td>
-							</tr>
+              <tr>
+                <td>[2021-2021]</td>
+                <td>
+                  <b>LegalAI </b>
+                  <div className="mobile">Web & DevOps Intern</div>
+                </td>
+                <td className="full">Web & DevOps Intern</td>
+              </tr>
 
-							<tr>
-								<td>[2018-2022]</td>
-								<td>
-									<b>Manipal Institute of Technology </b>
-									<div className="mobile">
-										B.Tech (IT)
-									</div>
-								</td>
-								<td className="full">
-									B.Tech (IT)
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<hr />
-			<table className="news-box">
-				<tbody>
-					{
-						news.map((x, idx) => <tr key={idx}>
-							<td>{idx == 0 ? <b><u>NEWS</u></b> : <></>}</td>
-							<td>
-								<span dangerouslySetInnerHTML={{ __html: x }}></span>
-							</td>
-						</tr>)
-					}
-				</tbody>
-			</table>
-
-		</>
-	);
+              <tr>
+                <td>[2018-2022]</td>
+                <td>
+                  <b>Manipal Institute of Technology </b>
+                  <div className="mobile">B.Tech (IT)</div>
+                </td>
+                <td className="full">B.Tech (IT)</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <hr />
+      <table className="news-box">
+        <tbody>
+          {news.map((x, idx) => (
+            <tr key={idx}>
+              <td>
+                {idx == 0 ? (
+                  <b>
+                    <u>NEWS</u>
+                  </b>
+                ) : (
+                  <></>
+                )}
+              </td>
+              <td
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <span
+                  style={{ margin: "0px", padding: "0px" }}
+                  dangerouslySetInnerHTML={{ __html: x[0] }}
+                ></span>
+                <span
+                  style={{
+                    margin: "0px",
+                    padding: "0px",
+                    color: "var(--text-color-alt)",
+                  }}
+                  dangerouslySetInnerHTML={{ __html: x[1] }}
+                ></span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
